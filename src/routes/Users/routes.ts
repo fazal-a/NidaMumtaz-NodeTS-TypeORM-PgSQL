05496029b54418dfa4db47cb9139a9655e44792a
fazal-a <thefazalabbas@gmail.com>
2024-02-09 05:45:13 +0500

    added authentication middleware

--- src/routes/Users/routes.ts
+++ src/routes/Users/routes.ts
@@ -1,16 +1,13 @@
-// src/routes/userRoutes.ts
-
 import express from 'express';
 import UserController from '../../controllers/Users/UserController';
+import AuthenticationMiddleware from "../../middleware/AuthenticationMiddleware";
 
 const userRouter = express.Router();
 
 userRouter.get('/getUser', UserController.getUser);
 userRouter.post('/createUser', UserController.createUser);
-userRouter.delete('/delete', UserController.deleteUser);
-userRouter.put('/update', UserController.updateUser);
+userRouter.delete('/delete',AuthenticationMiddleware.isAuthentication, UserController.deleteUser);
+userRouter.put('/update',AuthenticationMiddleware.isAuthentication, UserController.updateUser);
 userRouter.get('/login', UserController.loginUser);
-
-// Add other routes for CRUD operations
 
 export default userRouter;
